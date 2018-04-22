b6290ad2e7d4f8fb43bdd29799d14e472a182424
Federico Feroldi <federico@teamdigitale.governo.it>
2018-04-22 08:40:20 +0200

    Moves all source under lib

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-import * as dates from "./lib/dates";
-import * as numbers from "./lib/numbers";
-import * as reporters from "./lib/reporters";
-import * as strings from "./lib/strings";
-import * as tuples from "./lib/tuples";
-import * as types from "./lib/types";
-
+import * as dates from "./dates";
 export { dates };
+
+import * as numbers from "./numbers";
 export { numbers };
-export { strings };
-export { tuples };
-export { types };
+
+import * as reporters from "./reporters";
 export { reporters };
+
+import * as strings from "./strings";
+export { strings };
+
+import * as tuples from "./tuples";
+export { tuples };
+
+import * as types from "./types";
+export { types };
