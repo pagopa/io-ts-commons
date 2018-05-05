91b7170def4b4c109efe337e2120b3307fa1dbe6
Danilo Spinelli <gunzip@users.noreply.github.com>
2018-05-05 09:31:22 +0200

    [#157318099] added new type UrlFromString (#10)
    * [#157318099] added new type UrlFromString
    
    * fix: docs and tests
    
    * fix: tests
    
    * fix: tests

---
+++ src/__tests__/url.test.ts
@@ -0,0 +1,33 @@
+import { isLeft, isRight } from "fp-ts/lib/Either";
+import { HttpsUrlFromString, HttpUrlFromString, UrlFromString } from "../url";
+
+const anHostName = "example.com";
+const anHttpUrl = "http://" + anHostName;
+const anHttpsUrl = "https://" + anHostName;
+
+describe("UrlFromString", () => {
+  it("should get an href with a valid http URL", () => {
+    const errorOrUrl = UrlFromString.decode(anHttpUrl);
+    expect(isRight(errorOrUrl)).toBeTruthy();
+    if (isRight(errorOrUrl)) {
+      expect(errorOrUrl.value.href).toEqual(anHttpUrl + "/");
+      expect(errorOrUrl.value.hostname).toEqual(anHostName);
+    }
+  });
+  it("should fail on invalid http URL", () => {
+    const errorOrUrl = HttpUrlFromString.decode(anHttpsUrl);
+    expect(isLeft(errorOrUrl)).toBeTruthy();
+  });
+  it("should fail on invalid https URL", () => {
+    const errorOrUrl = HttpsUrlFromString.decode(anHttpUrl);
+    expect(isLeft(errorOrUrl)).toBeTruthy();
+  });
+  it("should pass on valid http URL", () => {
+    const errorOrUrl = HttpUrlFromString.decode(anHttpUrl);
+    expect(isRight(errorOrUrl)).toBeTruthy();
+  });
+  it("should pass on valid https URL", () => {
+    const errorOrUrl = HttpsUrlFromString.decode(anHttpsUrl);
+    expect(isRight(errorOrUrl)).toBeTruthy();
+  });
+});
