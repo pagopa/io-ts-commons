9f0450e7b120f6bb3560737dce73a5399d639fb6
Federico Feroldi <cloudify@users.noreply.github.com>
2018-05-16 11:26:14 +0200

    Adds API for making typesafe requests (#22)

---
+++ src/requests.ts
@@ -0,0 +1,361 @@
+/**
+ * Type safe wrapper around the fetch API
+ */
+
+// TODO: add body type parameter to POST/PUT
+// TODO: support for optional query parameters
+// TODO: when query/headers type is "never", it should not allow any query/header to be produced
+// TODO: add etag support in responses
+
+// The type definition of fetch is needed because it's not provided by TypeScript
+export declare function fetch(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<Response>;
+
+import * as t from "io-ts";
+
+/**
+ * Describes the possible methods of a request
+ */
+export type RequestMethod = "get" | "post" | "put";
+
+/**
+ * Describes the possible header keys of a request
+ */
+// tslint:disable-next-line:no-duplicate-string
+export type RequestHeaderKey = "Authorization" | "Content-Type";
+
+/**
+ * Describes a set of headers whose keys are of type RequestHeaderKey
+ */
+export type RequestHeaders<HS extends RequestHeaderKey> = {
+  [key in HS]: string
+};
+
+/**
+ * Describes the query params for this request
+ */
+export type RequestQuery<K extends string> = Record<K, string>;
+
+/**
+ * Generates a set of headers with certain keys (KH) from a parameters object
+ * of type P.
+ */
+export type RequestHeaderProducer<P, KH extends RequestHeaderKey> = (
+  params: P
+) => RequestHeaders<KH>;
+
+/**
+ * Composes two RequestHeaderProducer(s)
+ */
+export function composeHeaderProducers<
+  PI,
+  KI extends RequestHeaderKey,
+  PH,
+  KH extends RequestHeaderKey
+>(
+  p0: RequestHeaderProducer<PI, KI>,
+  p1: RequestHeaderProducer<PH, KH>
+): RequestHeaderProducer<PI & PH, KI | KH> {
+  return params => {
+    const headers0 = p0(params);
+    const headers1 = p1(params);
+    return {
+      // tslint:disable-next-line:no-any
+      ...(headers0 as any),
+      // tslint:disable-next-line:no-any
+      ...(headers1 as any)
+    };
+  };
+}
+
+/**
+ * Describes a possible response type: when the status code is S, the response
+ * type is T.
+ */
+export interface IResponseType<S extends number, T> {
+  readonly status: S;
+  readonly value: T;
+}
+
+/**
+ * A function that generates a typed representation of a response.
+ * It should return undefined in case the response cannot be decoded (e.g.
+ * in case of a parsing error).
+ */
+export type ResponseDecoder<R> = (response: Response) => Promise<R | undefined>;
+
+/**
+ * Composes two ResponseDecoder(s)
+ */
+export function composeResponseDecoders<R1, R2>(
+  d1: ResponseDecoder<R1>,
+  d2: ResponseDecoder<R2>
+): ResponseDecoder<R1 | R2> {
+  // TODO: make sure R1, R2 don't intersect
+  return async response => {
+    const r1 = await d1(response);
+    return r1 !== undefined ? r1 : await d2(response);
+  };
+}
+
+/**
+ * Fully describes an API request.
+ *
+ * @param M   The request method
+ * @param P   The type of input parameters
+ * @param H   The headers that must be defined for this request
+ * @param R   The possible response types for this request
+ */
+export interface IBaseApiRequestType<
+  M extends RequestMethod,
+  P,
+  H extends RequestHeaderKey,
+  Q extends string,
+  R
+> {
+  readonly method: M;
+  readonly url: (params: P) => string;
+  readonly query: (params: P) => RequestQuery<Q>;
+  readonly headers: RequestHeaderProducer<P, H>;
+  readonly response_decoder: ResponseDecoder<R>;
+}
+
+/**
+ * Fully describes a GET request.
+ */
+export interface IGetApiRequestType<
+  P,
+  KH extends RequestHeaderKey,
+  Q extends string,
+  R
+> extends IBaseApiRequestType<"get", P, KH, Q, R> {
+  readonly method: "get";
+}
+
+/**
+ * Fully describes a POST request.
+ *
+ * POST requests require to provide the "Content-Type" header.
+ */
+export interface IPostApiRequestType<
+  P,
+  KH extends RequestHeaderKey,
+  Q extends string,
+  R
+> extends IBaseApiRequestType<"post", P, KH | "Content-Type", Q, R> {
+  readonly method: "post";
+  readonly body: (params: P) => string;
+}
+
+/**
+ * Fully describes a PUT request.
+ *
+ * PUT requests require to provide the "Content-Type" header.
+ */
+export interface IPutApiRequestType<
+  P,
+  KH extends RequestHeaderKey,
+  Q extends string,
+  R
+> extends IBaseApiRequestType<"put", P, KH | "Content-Type", Q, R> {
+  readonly method: "put";
+  readonly body: (params: P) => string;
+}
+
+export type ApiRequestType<
+  P,
+  KH extends RequestHeaderKey,
+  Q extends string,
+  R
+> =
+  | IGetApiRequestType<P, KH, Q, R>
+  | IPostApiRequestType<P, KH, Q, R>
+  | IPutApiRequestType<P, KH, Q, R>;
+
+//
+// helpers
+//
+
+function queryStringFromParams<P extends string>(
+  params: RequestQuery<P>
+): string | undefined {
+  const keys = Object.getOwnPropertyNames(params);
+  if (keys.length === 0) {
+    return undefined;
+  }
+  return keys
+    .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k as P])}`)
+    .join("&");
+}
+
+/**
+ * Returns an async method that implements the provided ApiRequestType backed
+ * by the "fetch" API.
+ */
+export function createFetchRequestForApi<
+  P,
+  KH extends RequestHeaderKey,
+  Q extends string,
+  R
+>(
+  requestType: ApiRequestType<P, KH, Q, R>,
+  options: {
+    readonly baseUrl?: string;
+    readonly fetch?: typeof fetch;
+  } = {}
+): (params: P) => Promise<R | undefined> {
+  // TODO: handle unsuccessful fetch and HTTP errors
+  // @see https://www.pivotaltracker.com/story/show/154661120
+  return async params => {
+    // Build operationUrl from the params
+    const operationUrl = requestType.url(params);
+
+    // Build request url from baseUrl if provided
+    const requestUrl = options.baseUrl
+      ? options.baseUrl +
+        (options.baseUrl.endsWith("/") || operationUrl.startsWith("/")
+          ? ""
+          : "/") +
+        operationUrl
+      : operationUrl;
+
+    // Generate the query params
+    const queryParams = requestType.query(params);
+    const queryString = queryStringFromParams(queryParams);
+
+    // Append the query params to the URL
+    const url =
+      queryString === undefined ? requestUrl : `${requestUrl}?${queryString}`;
+
+    // get the headers from the params
+    const headers = requestType.headers.apply(params);
+
+    // build the request
+    const baseRequest: RequestInit = {
+      method: requestType.method
+    };
+
+    const requestWithOptionalHeaders =
+      Object.keys(headers).length === 0
+        ? baseRequest
+        : {
+            ...baseRequest,
+            headers
+          };
+
+    const requestWithOptionalHeadersAndBody =
+      requestType.method === "get"
+        ? requestWithOptionalHeaders
+        : {
+            ...requestWithOptionalHeaders,
+            body: requestType.body(params)
+          };
+
+    // Get the fetch client
+    const myFetch = options.fetch ? options.fetch : fetch;
+
+    // make the async call
+    const response = await myFetch(url, requestWithOptionalHeadersAndBody);
+
+    // decode the response
+    return requestType.response_decoder(response);
+  };
+}
+
+/**
+ * An header producer that sets the Content-Type to application/json
+ */
+export const ApiHeaderJson: RequestHeaderProducer<{}, "Content-Type"> = () => ({
+  "Content-Type": "application/json"
+});
+
+/**
+ * An io-ts based ResponseDecoder, it checks the response status and the
+ * payload against the provided type.
+ *
+ * @param status  The response status handled by this decoder
+ * @param type    The response type corresponding to the status
+ */
+export function ioResponseDecoder<S extends number, R>(
+  status: S,
+  type: t.Type<R>
+): ResponseDecoder<IResponseType<S, R>> {
+  return async (response: Response) => {
+    if (response.status !== status) {
+      return undefined;
+    }
+    const json = await response.json();
+    const validated = type.decode(json);
+    if (validated.isLeft()) {
+      return undefined;
+    }
+    return { status, value: validated.value };
+  };
+}
+
+/**
+ * A basic ResponseDecoder that returns an Error with the status text if the
+ * response status is S.
+ */
+export function basicErrorResponseDecoder<S extends number>(
+  status: S
+): ResponseDecoder<IResponseType<S, Error>> {
+  return async response => {
+    if (response.status !== status) {
+      return undefined;
+    }
+    return { status, value: new Error(response.statusText) };
+  };
+}
+
+/**
+ * A basic set of responses where the 200 status corresponds to a payload of
+ * type R and 404 and 500 to an Error
+ */
+export type BasicResponseType<R> =
+  | IResponseType<200, R>
+  | IResponseType<404, Error>
+  | IResponseType<500, Error>;
+
+/**
+ * Returns a ResponseDecoder for BasicResponseType<R>
+ */
+export function basicResponseDecoder<R>(
+  type: t.Type<R>
+): ResponseDecoder<BasicResponseType<R>> {
+  return composeResponseDecoders(
+    composeResponseDecoders(
+      ioResponseDecoder(200, type),
+      basicErrorResponseDecoder(404)
+    ),
+    basicErrorResponseDecoder(500)
+  );
+}
+
+/**
+ * Returns a RequestHeaderProducer that produces an Authorization header of type
+ * "bearer token" taking the value from the "token" parameter of each request.
+ */
+export function ParamAuthorizationBearerHeaderProducer<
+  P extends { readonly token: string }
+>(): RequestHeaderProducer<P, "Authorization"> {
+  return (p: P): RequestHeaders<"Authorization"> => {
+    return {
+      Authorization: `Bearer ${p.token}`
+    };
+  };
+}
+
+/**
+ * Returns a RequestHeaderProducer that produces an Authorization header of type
+ * "bearer token" with a fixed token value.
+ */
+export function AuthorizationBearerHeaderProducer<P>(
+  token: string
+): RequestHeaderProducer<P, "Authorization"> {
+  return () => ({
+    Authorization: `Bearer ${token}`
+  });
+}
