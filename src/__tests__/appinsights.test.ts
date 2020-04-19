6b57b955fd28c7c38ba9627c72574a2b2b9831af
danilo.spinelli <danilo.spinelli@pagopa.it>
2020-04-19 18:25:04 +0200

    fix tests

--- src/__tests__/appinsights.test.ts
+++ src/__tests__/appinsights.test.ts
@@ -1,13 +1,6 @@
 import * as appInsights from "applicationinsights";
 import { Configuration } from "applicationinsights";
-import {
-  initAppInsights,
-  removeQueryParamsPreprocessor,
-  withAppInsightsContext
-} from "../appinsights";
-
-// tslint:disable-next-line: no-submodule-imports
-import { CorrelationContextManager } from "applicationinsights/out/AutoCollection/CorrelationContextManager";
+import { initAppInsights, removeQueryParamsPreprocessor } from "../appinsights";
 
 describe("Create an App Insights Telemetry Client", () => {
   const mockSetup = jest.spyOn(appInsights, "setup");
@@ -83,51 +76,6 @@ describe("Create an App Insights Telemetry Client", () => {
     expect(telemetryClient).toEqual(appInsights.defaultClient);
     expect(telemetryClient.config.httpAgent).not.toBeUndefined();
     expect(telemetryClient.config.samplingPercentage).toBe(20);
-  });
-});
-
-describe("Wrap an handler with Application Insights context", () => {
-  const CollerationContextSpy = jest.spyOn(
-    CorrelationContextManager,
-    "generateContextObject"
-  );
-
-  it("should return the handler value", async () => {
-    const handler = (a: string, b: number) => `${a}${b}`;
-    const ret = withAppInsightsContext(
-      {
-        executionContext: {
-          functionName: "foo"
-        },
-        invocationId: "123"
-        // tslint:disable-next-line: no-any
-      } as any,
-      () => handler("bar", 2)
-    );
-    expect(CollerationContextSpy).toHaveBeenCalledWith("123", "123", "foo");
-    expect(ret).toEqual("bar2");
-  });
-  it("should set traceId to traceparent.traceId", async () => {
-    const handler = (a: string, b: number) => `${a}${b}`;
-    const ret = withAppInsightsContext(
-      {
-        executionContext: {
-          functionName: "foo"
-        },
-        invocationId: "123",
-        traceContext: {
-          traceparent: "00-0af7651916cd43dd8448eb211c80319c-b7ad6b7169203331-01"
-        }
-        // tslint:disable-next-line: no-any
-      } as any,
-      () => handler("bar", 2)
-    );
-    expect(CollerationContextSpy).toHaveBeenCalledWith(
-      "0af7651916cd43dd8448eb211c80319c",
-      "0af7651916cd43dd8448eb211c80319c",
-      "foo"
-    );
-    expect(ret).toEqual("bar2");
   });
 });
 
