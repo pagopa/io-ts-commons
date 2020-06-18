b605e0f5971d767ce5a092c412d560de08fe866c
Emanuele De Cupis <emanuele@MacBook-Pro.station>
2020-06-18 11:06:23 +0200

    add time units

--- src/units.ts
+++ src/units.ts
@@ -9,3 +9,9 @@ export interface IUnitTag<T> {
 export type Millisecond = number & IUnitTag<"Millisecond">;
 
 export type Second = number & IUnitTag<"Second">;
+
+export type Minute = number & IUnitTag<"Minute">;
+
+export type Hour = number & IUnitTag<"Hour">;
+
+export type Day = number & IUnitTag<"Day">;
