270c14e559a2c0372502961f1a12a6933bb50ae1
Michael Disaro <michael.disaro@gmail.com>
2021-09-06 18:38:10 +0200

    added codec to support conversion from string to boolean

---
+++ src/booleans.ts
@@ -0,0 +1,26 @@
+import { pipe } from "fp-ts/lib/function";
+import * as E from "fp-ts/lib/Either";
+import * as t from "io-ts";
+
+export interface BooleanFromString extends t.Type<boolean, string, unknown> {}
+
+export const BooleanFromString: BooleanFromString = new t.Type<
+  boolean,
+  string,
+  unknown
+>(
+  "BooleanFromString",
+  t.boolean.is,
+  (u, c) =>
+    pipe(
+      t.string.validate(u, c),
+      E.chain(s =>
+        s === "true"
+          ? t.success(true)
+          : s === "false"
+          ? t.success(false)
+          : t.failure(u, c)
+      )
+    ),
+  String
+);
