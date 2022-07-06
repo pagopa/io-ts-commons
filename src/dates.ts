90d5983c669d499ff34b78581fb3a7ea042ae950
fabrizio.papi <fabrizio.papi@pagopa.it>
2022-07-06 15:07:01 +0200

    add timezone support to ISO Date

--- src/dates.ts
+++ src/dates.ts
@@ -27,39 +27,85 @@ export const DateFromString = new t.Type<Date, string>(
 
 export type DateFromString = t.TypeOf<typeof DateFromString>;
 
+export const patternDateFromString = (
+  pattern: string,
+  tagName: string
+): t.Type<Date, string> =>
+  new t.Type<Date, string>(
+    tagName,
+    isDate,
+    (v, c) =>
+      isDate(v)
+        ? t.success(v)
+        : pipe(
+            PatternString(pattern).validate(v, c),
+            E.chain(s => {
+              const d = new Date(s);
+              return isNaN(d.getTime()) ? t.failure(s, c) : t.success(d);
+            })
+          ),
+    a => a.toISOString()
+  );
+
 /**
  * ISO8601 format for dates.
  *
+ */
+/**
  * Date and time is separated with a capital T.
  * UTC time is defined with a capital letter Z.
  *
  */
-const UTC_ISO8601_FULL_REGEX = PatternString(
-  "^\\d{4}-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d(\\.\\d+)?(Z)?$"
-);
+const UTC_ISO8601_FULL_REGEX =
+  "^\\d{4}-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d(\\.\\d+)?(Z)?$";
+/**
+ * Date and time is separated with a capital T.
+ * Timezone is defined with +/-00:00 format.
+ *
+ */
+const TIMEZONE_ISO8601_FULL_REGEX =
+  "^\\d{4}-\\d\\d-\\d\\dT\\d\\d:\\d\\d:\\d\\d(\\.\\d+)?[+-](\\d{2})\\:(\\d{2})$";
 
 /**
  * Accepts only full ISO8601 format with UTC (Z) timezone
  *
  * ie. "2018-10-13T00:00:00.000Z"
  */
-export const UTCISODateFromString = new t.Type<Date, string>(
-  "UTCISODateFromString",
-  isDate,
-  (v, c) =>
-    isDate(v)
-      ? t.success(v)
-      : pipe(
-          UTC_ISO8601_FULL_REGEX.validate(v, c),
-          E.chain(s => {
-            const d = new Date(s);
-            return isNaN(d.getTime()) ? t.failure(s, c) : t.success(d);
-          })
-        ),
-  a => a.toISOString()
+export const UtcOnlyIsoDateFromString = patternDateFromString(
+  UTC_ISO8601_FULL_REGEX,
+  "UtcOnlyIsoDateFromString"
 );
+export type UtcOnlyIsoDateFromString = t.TypeOf<
+  typeof UtcOnlyIsoDateFromString
+>;
 
-export type UTCISODateFromString = t.TypeOf<typeof UTCISODateFromString>;
+/**
+ * Accepts only full ISO8601 format with +/-00:00 timezone
+ *
+ * ie. "2018-10-13T00:00:00.000+00:00"
+ */
+export const TimezoneOnlyIsoDateFromString = patternDateFromString(
+  TIMEZONE_ISO8601_FULL_REGEX,
+  "TimezoneOnlyIsoDateFromString"
+);
+export type TimezoneOnlyIsoDateFromString = t.TypeOf<
+  typeof TimezoneOnlyIsoDateFromString
+>;
+
+export const IsoDateFromString = t.union(
+  [UtcOnlyIsoDateFromString, TimezoneOnlyIsoDateFromString],
+  "IsoDateFromString"
+);
+export type IsoDateFromString = typeof IsoDateFromString;
+
+/**
+ * @deprecated use IsoDateFromString instead.
+ */
+export const UTCISODateFromString = IsoDateFromString;
+/**
+ * @deprecated use IsoDateFromString instead.
+ */
+export type UTCISODateFromString = typeof UTCISODateFromString;
 
 /**
  * Accepts only a valid timestamp format (number)
