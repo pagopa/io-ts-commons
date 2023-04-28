3742559740064c6fc5de7248ad7060ea9a7b9ca2
Daniele Manni <dan.manni@gmail.com>
2023-04-28 11:02:55 +0200

    Upgrade dependencies: fp-ts, io-ts, typescrypt, eslint

--- Dangerfile.ts
+++
@@ -1,7 +0,0 @@
-// import custom DangerJS rules
-// see http://danger.systems/js
-// see https://github.com/teamdigitale/danger-plugin-digitalcitizenship/
-// eslint-disable-next-line prettier/prettier
-import checkDangers from "danger-plugin-digitalcitizenship";
-
-checkDangers();
