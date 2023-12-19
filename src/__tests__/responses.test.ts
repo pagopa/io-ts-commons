35d267e5fe4fd21db5555bd8cbfd23f8ebb0e44a
Daniele Manni <BurnedMarshal@users.noreply.github.com>
2023-12-19 11:21:39 +0100

    [#IOPID-1268] Addressable type ResponseErrorPreconditionFailed (#417)

--- src/__tests__/responses.test.ts
+++ src/__tests__/responses.test.ts
@@ -1,20 +1,28 @@
-import { ResponseSuccessJson } from "../responses";
+import {
+  HttpStatusCodeEnum,
+  ResponseErrorPreconditionFailed,
+  ResponseSuccessJson,
+} from "../responses";
 
 import MockResponse from "../__mocks__/response";
 
-import { getResponseErrorForbiddenNoAuthorizationGroups, ResponseErrorForbiddenNoAuthorizationGroups, ResponseErrorForbiddenNotAuthorized, getResponseErrorForbiddenNotAuthorized} from "../responses";
+import {
+  getResponseErrorForbiddenNoAuthorizationGroups,
+  ResponseErrorForbiddenNoAuthorizationGroups,
+  ResponseErrorForbiddenNotAuthorized,
+  getResponseErrorForbiddenNotAuthorized,
+} from "../responses";
 
 describe("ResponseSuccessJson", () => {
-
   it("should remove the kind property", () => {
     const kindlessData = {
       a: 1,
-      b: "2"
+      b: "2",
     };
 
     const kindedData = {
       ...kindlessData,
-      kind: "I_AM_UNIQUE"
+      kind: "I_AM_UNIQUE",
     };
 
     const mockResponse = MockResponse();
@@ -28,35 +36,103 @@ describe("ResponseSuccessJson", () => {
 });
 
 describe("ResponseErrorForbiddenNotAuthorized", () => {
-
   it("should return the standard response", () => {
-    expect(getResponseErrorForbiddenNotAuthorized())
-      .toMatchObject(expect.objectContaining({kind: "IResponseErrorForbiddenNotAuthorized", detail: expect.stringContaining("You do not have enough permission to complete the operation you requested")}));
-    expect(ResponseErrorForbiddenNotAuthorized)
-      .toMatchObject(expect.objectContaining({kind: "IResponseErrorForbiddenNotAuthorized", detail: expect.stringContaining("You do not have enough permission to complete the operation you requested")}));
-  })
+    expect(getResponseErrorForbiddenNotAuthorized()).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorForbiddenNotAuthorized",
+        detail: expect.stringContaining(
+          "You do not have enough permission to complete the operation you requested"
+        ),
+      })
+    );
+    expect(ResponseErrorForbiddenNotAuthorized).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorForbiddenNotAuthorized",
+        detail: expect.stringContaining(
+          "You do not have enough permission to complete the operation you requested"
+        ),
+      })
+    );
+  });
 
   it("should return a response with a custom detail", () => {
     const customDetail = "aCustomDetail";
-    expect(getResponseErrorForbiddenNotAuthorized(customDetail))
-      .toMatchObject(expect.objectContaining({kind: "IResponseErrorForbiddenNotAuthorized", detail: expect.stringContaining(customDetail)}));
-  })
-
-})
+    expect(getResponseErrorForbiddenNotAuthorized(customDetail)).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorForbiddenNotAuthorized",
+        detail: expect.stringContaining(customDetail),
+      })
+    );
+  });
+});
 
 describe("ResponseErrorForbiddenNoAuthorizationGroups ", () => {
-
   it("should return the standard response", () => {
-    expect(getResponseErrorForbiddenNoAuthorizationGroups())
-      .toMatchObject(expect.objectContaining({kind: "IResponseErrorForbiddenNoAuthorizationGroups", detail: expect.stringContaining("You are not part of any valid scope, you should ask the administrator to give you the required permissions.")}));
-    expect(ResponseErrorForbiddenNoAuthorizationGroups)
-      .toMatchObject(expect.objectContaining({kind: "IResponseErrorForbiddenNoAuthorizationGroups", detail: expect.stringContaining("You are not part of any valid scope, you should ask the administrator to give you the required permissions.")}));
-  })
+    expect(getResponseErrorForbiddenNoAuthorizationGroups()).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorForbiddenNoAuthorizationGroups",
+        detail: expect.stringContaining(
+          "You are not part of any valid scope, you should ask the administrator to give you the required permissions."
+        ),
+      })
+    );
+    expect(ResponseErrorForbiddenNoAuthorizationGroups).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorForbiddenNoAuthorizationGroups",
+        detail: expect.stringContaining(
+          "You are not part of any valid scope, you should ask the administrator to give you the required permissions."
+        ),
+      })
+    );
+  });
 
   it("should return a response with a custom detail", () => {
     const customDetail = "aCustomDetail";
-    expect(getResponseErrorForbiddenNoAuthorizationGroups(customDetail))
-      .toMatchObject(expect.objectContaining({kind: "IResponseErrorForbiddenNoAuthorizationGroups", detail: expect.stringContaining(customDetail)}));
-  })
+    expect(
+      getResponseErrorForbiddenNoAuthorizationGroups(customDetail)
+    ).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorForbiddenNoAuthorizationGroups",
+        detail: expect.stringContaining(customDetail),
+      })
+    );
+  });
+});
 
-})
+describe("ResponseErrorPreconditionFailed", () => {
+  const expectedDetail = "The precondition are failed";
+  it("should return the standard response with a detail", () => {
+    expect(ResponseErrorPreconditionFailed(expectedDetail)).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorPreconditionFailed",
+        detail: expect.stringContaining(expectedDetail),
+      })
+    );
+  });
+
+  it("should return a response with detail and custom problemType", () => {
+    const customProblemType = "https://customproblemtype.com";
+    const mockResponse = MockResponse();
+    const response = ResponseErrorPreconditionFailed(
+      expectedDetail,
+      customProblemType
+    );
+    expect(response).toMatchObject(
+      expect.objectContaining({
+        kind: "IResponseErrorPreconditionFailed",
+        detail: expect.stringContaining(expectedDetail),
+      })
+    );
+
+    response.apply(mockResponse);
+
+    expect(mockResponse.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        status: HttpStatusCodeEnum.HTTP_STATUS_412,
+        type: customProblemType,
+        detail: expect.stringContaining(expectedDetail),
+        title: "Precondition Failed",
+      })
+    );
+  });
+});
