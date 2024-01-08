3b5d06dee30ea82ba05b9028715747b0f3057a2f
Luca Cavallaro <4357400+lucacavallaro@users.noreply.github.com>
2024-01-08 16:01:30 +0100

    fix lint

--- src/hash.ts
+++ src/hash.ts
@@ -1,8 +1,8 @@
-import { FiscalCode } from "./strings";
-
 import * as crypto from "crypto";
 
-export const hashFiscalCode = (fiscalCode: FiscalCode) => {
+import { FiscalCode } from "./strings";
+
+export const hashFiscalCode = (fiscalCode: FiscalCode): string => {
   const hash = crypto.createHash("sha256");
   hash.update(fiscalCode);
   return hash.digest("hex");
